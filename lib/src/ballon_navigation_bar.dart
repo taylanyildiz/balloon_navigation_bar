31b2c1c62b5d68544677232bd901f3cf0ebb9497
Taylan YILDIZ <taylanyildz@outlook.com>
2022-03-15 23:05:36 +0300

    for road to IZMIR

--- lib/src/ballon_navigation_bar.dart
+++ lib/src/ballon_navigation_bar.dart
@@ -1,36 +1,65 @@
 import 'package:flutter/material.dart';
 
+const _defaultBackgroundColor = Color(0xff161F35);
+const _defaultSelectedIconTheme = IconThemeData(
+  color: Colors.white,
+  size: 25.0,
+);
+const _defaultUnSelectedIconTheme = IconThemeData(
+  color: Color(0xff526187),
+  size: 25.0,
+);
+
 /// Balon butonu bulunduran ve bununla beraber 4 widget ile beraber bir navigation bardır.
 /// Balon buton animasyon içerir ve özelleştirilmesine izin verilir
 /// Fluuter'in sunduğu [BottomNavigationBarThemeData] Temayı destekler sadece bir kaç farklılık bulundurmaktadır.
-/// örneğin
-///
+/// Sadece iconlar için geçerlidir.
+/// örneğin :
 /*
 
-     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color(0xff161F35),             # Bottom barın arklaplan rengi
-          selectedItemColor: Colors.white,                # Balon buton ve itemlarının önplan rengi
-          unselectedItemColor: Color(0xff1da2ff),         # Balon butonun ve itemlarının arkaplan rengi
-          selectedIconTheme: IconThemeData(               # diğer 4 widgetin iconThemeDatası
-            color: Colors.white,
-            size: 25.0,
-          ),
-          unselectedIconTheme: IconThemeData(             # diğer 4 widgetin iconThemeDatası
-            color: Color(0xff526187),
-            size: 25.0,
-          ),
-        ),
+ [BottomNavigationBarThemeData]: 
+[backgroundColor]:      # Bottom barın arklaplan rengi
+[selectedItemColor]:    # Balon buton ve itemlarının önplan rengi
+[unselectedItemColor]:  # Balon butonun ve itemlarının arkaplan rengi
+[selectedIconTheme]:    # diğer 4 widgetin iconThemeDatası
+[unselectedIconTheme]:  # diğer 4 widgetin iconThemeDatası
 
 */
 /// Tema örneğini anlamadıysanız [örnek]() koda bakabilirsiniz
 class BallonNavigationBar extends StatefulWidget {
-  const BallonNavigationBar({Key? key}) : super(key: key);
+  const BallonNavigationBar({
+    Key? key,
+  }) : super(key: key);
+
+  ///
+  final List<Widget> items;
+
+  ///
+  final Color backgroundColor;
+
+  ///
+  final Color selectedIconColor;
+
+  ///
+  final Color unSelectedIconColor;
+
+  ///
+  final double evelation;
 
   @override
   State<BallonNavigationBar> createState() => _BallonNavigationBarState();
 }
 
 class _BallonNavigationBarState extends State<BallonNavigationBar> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  ThemeData _theme(BuildContext context) => Theme.of(context);
+  BottomNavigationBarThemeData _barThemeData(BuildContext context) =>
+      _theme(context).bottomNavigationBarTheme;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -43,7 +72,9 @@ class _BallonNavigationBarState extends State<BallonNavigationBar> {
   }
 
   BoxDecoration get _boxDecoration {
-    return const BoxDecoration(color: Colors.red);
+    return const BoxDecoration(
+      color: Colors.red,
+    );
   }
 
   Widget get _child {
